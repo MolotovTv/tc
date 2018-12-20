7bdc7eef66dfbf1b99c025b7d23e8689971eb6ed
Jean-Baptiste PINALIE <2850825+jybp@users.noreply.github.com>
2018-12-20 16:57:45 +0100

    hello

--- cmd/root.go
+++ cmd/root.go
@@ -1,12 +1,11 @@
 package cmd
 
 import "github.com/spf13/cobra"
-import "fmt"
 
 var RootCmd = &cobra.Command{
 	Use:   "tc",
 	Short: "TeamCity",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("hello")
+		cmd.Help()
 	},
 }
